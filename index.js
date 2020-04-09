b4c4009d0aec8c63c1891f018274845d3d6981fa
Yeng Vang <yenghuevang@hotmail.com>
2020-04-08 20:03:36 -0500

    get data table working

--- index.js
+++ index.js
@@ -1,11 +1,60 @@
 const connection = require('./connection.js');
-// const db = require('/db.sql');
+const inquirer = require("inquirer");
+
+function start(){
+
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "What would you like to do?",
+                name: "userInput",
+                choices: [
+                    "View Departments",
+                    "View Roles",
+                    "View Employees",
+                    "Add Department",
+                    "Add Role",
+                    "Add Employee"
+                ]
+            }
+        ])
+        .then(function ({userInput}){
+            switch(userInput){
+                case "View Departments":
+                    getAlldepartments();
+                    break;
+                
+                case "View Roles":
+                    getAllroles();
+                    break;
+                
+                case "View Employees":
+                    getAllemployees();
+                    break;
+
+                case "Add Department":
+                    addDepartments();
+                    break;
+                
+                case "Add Role":
+                    addRoles();
+                    break;
+
+                case "Add Employee":
+                    addEmployees();
+                    break;
+            }
+        })
+}
+
 
 
 function getAlldepartments(){
         return connection.query('SELECT * FROM department')
            .then(results => {
-             console.table(results)
+             console.table(results);
+             endProgram();
           })
           .catch(err => console.log(err))
     };
@@ -14,14 +63,16 @@ function getAllroles(){
         return connection.query('SELECT * FROM roles')
            .then(results => {
              console.table(results)
+             endProgram();
           })
           .catch(err => console.log(err))
     };
 
-function getAllemployee(){
+function getAllemployees(){
         return connection.query('SELECT * FROM employees')
            .then(results => {
              console.table(results)
+             endProgram();
           })
           .catch(err => console.log(err))
     };
@@ -43,8 +94,31 @@ function addEmployees(){
 
 };
 
+function endProgram(){
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Do you want to continue?",
+                name: "choice",
+                choices: [
+                    "Yes, back to the start.",
+                    "No, done with changes."
+                ]
+            }
+        ])
+        .then(function ({choice}){
+            if(choice === "Yes, back to the start."){
+                start();
+            }
+            else{
+                connection.end();
+            }
+        })
+}
+
 // getAlldepartments()
 // getAllemployee()
 // getAllroles()
 
-addDepartments(sales)
+start()
